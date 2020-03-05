a811ea61bd71d8c01a22ff81414e2f26ea8e5329
Kirill Shcherbina <rabbichaim770@gmail.com>
2020-03-05 02:09:04 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -5,11 +5,11 @@ chai.use( require( 'chai-fs' ) );
 
 describe( 'this lab', () => {
 
-  it( 'has a folder named my-repository', () => {
+  it( 'has a folder named my-repository', => {
     chai.assert.isDirectory( './my-repository', 'no folder name "my-repository" was found' );
   } )
 
-  it( 'has a valid git repository initialized for the my-repository folder', () => {
+  i( 'has a valid git repository initialized for the my-repository folder', () => {
     chai.assert.isDirectory( './my-repository/.git', 'no ".git" folder was found within "/my-repository, used "git init" to initialize' );
   } )
 
